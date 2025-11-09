470bb20faf4dbc3db360adaaf542ebe589db51ee
Tanner McCurdy <tmccurdy1800@gmail.com>
2025-11-09 09:54:21 -0700

    Assignment 9
    Added 3 js interactions

---
+++ javajam/js/verifysenior.js
@@ -0,0 +1,12 @@
+var age;
+
+age = prompt("What's Your Age:")
+if (age >= 65) {
+    output = "Free Friday Coffee Night for Old People!" 
+    document.getElementById('verify').style.color = 'red';
+    document.getElementById('verify').style.fontVariantCaps = 'small-caps';
+} else {
+    output = "Enjoy being Old!"
+}
+
+document.getElementById("verify").innerHTML = output;
